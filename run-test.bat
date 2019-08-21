cf3a81feafce7b9672841f61846ae07ee8b2c39a
mmh561 <mdmhasan99@yahoo.com>
2019-08-20 21:49:37 -0400

    this is my first commit

---
+++ run-test.bat
@@ -0,0 +1,2 @@
+cd C:\Users\MD Hasan\Documents\workspace-sts-3.9.7.RELEASE\MyFirstProject
+mvn clean install
---
+++ src/main/java/com/att/md/MyFirstProject/Practice.java
@@ -0,0 +1,14 @@
+package com.att.md.MyFirstProject;
+
+import org.testng.annotations.Test;
+
+public class Practice {
+	
+	@Test
+	public static void test() {
+
+		System.out.println("Hello World");
+	}
+
+
+}
